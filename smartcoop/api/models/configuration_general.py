a66e988f6d6c7eadd42c3981f2376a375f7232ac
“ben <ben.browning@omlet.co.uk>
2024-08-23 15:49:51 +0100

    made some of general config optional, due to fan not having a language, and always having mains power => not having an overnight sleep

--- smartcoop/api/models/configuration_general.py
+++ smartcoop/api/models/configuration_general.py
@@ -8,13 +8,13 @@ class ConfigurationGeneral:
     datetime: str
     timezone: str
     updateFrequency: int
-    language: str
-    overnightSleepEnable: bool
-    overnightSleepStart: str
-    overnightSleepEnd: str
     pollFreq: int
     stayAliveTime: int
     statusUpdatePeriod: int
+    language: Optional[str] = None
+    overnightSleepEnable: Optional[bool] = None
+    overnightSleepStart: Optional[str] = None
+    overnightSleepEnd: Optional[str] = None
     useDst: Optional[bool] = None
 
     @staticmethod
@@ -24,10 +24,10 @@ class ConfigurationGeneral:
             timezone=json_data['timezone'],
             useDst=json_data.get('useDst'),
             updateFrequency=json_data['updateFrequency'],
-            language=json_data['language'],
-            overnightSleepEnable=json_data['overnightSleepEnable'],
-            overnightSleepStart=json_data['overnightSleepStart'],
-            overnightSleepEnd=json_data['overnightSleepEnd'],
+            language=json_data.get('language'),
+            overnightSleepEnable=json_data.get('overnightSleepEnable'),
+            overnightSleepStart=json_data.get('overnightSleepStart'),
+            overnightSleepEnd=json_data.get('overnightSleepEnd'),
             pollFreq=json_data['pollFreq'],
             stayAliveTime=json_data['stayAliveTime'],
             statusUpdatePeriod=json_data['statusUpdatePeriod']
